ed89e4418ec08f222b609d869549f0c9f8cceb48
EmonMridha <emonmridha712@gmail.com>
2026-01-04 12:53:22 +0600

    added conditional navbar

--- src/pages/shared/navbar/Navbar.jsx
+++ src/pages/shared/navbar/Navbar.jsx
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
 import { Link } from "react-router";
+import useAuth from "../../../Hooks/useAuth";
+import Swal from "sweetalert2";
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
+    const { user, logOut } = useAuth();
+
+    const handleLogout = () => {
+        logOut()
+            .then(() => {
+                Swal.fire('LoggedOut Successfully')
+            })
+            .catch(() => {
+                Swal.fire();
+                ('Could not logOut')
+            })
+    }
 
     return (
         <nav className="bg-white text-black px-6 my-5 mx-10 rounded-2xl py-4 relative">
@@ -12,7 +26,7 @@ const Navbar = () => {
                 <div className="flex items-center gap-4">
                     {/* Hamburger */}
                     <button
-                        className="md:hidden text-2xl"
+                        className="lg:hidden cursor-pointer text-2xl"
                         onClick={() => setOpen(!open)}
                     >
                         ☰
@@ -26,37 +40,42 @@ const Navbar = () => {
                 </div>
 
                 {/* Center - Desktop */}
-                <ul className="hidden md:flex gap-10">
-                    <li><a className="hover:text-sky-400">Home</a></li>
-                    <li><a className="hover:text-sky-400">Services</a></li>
-                    <li><a className="hover:text-sky-400">Products</a></li>
-                    <li><a className="hover:text-sky-400">About</a></li>
-                    <li><a className="hover:text-sky-400">Contact</a></li>
+                <ul className="hidden lg:flex gap-10">
+                    <button><a className="hover:text-sky-400">Home</a></button>
+                    <button><a className="hover:text-sky-400">Services</a></button>
+                    <button><a className="hover:text-sky-400">Coverage</a></button>
+                    <button><a className="hover:text-sky-400">TRackOrder</a></button>
+                    <button><a className="hover:text-sky-400">Pricing</a></button>
+                    <button><a className="hover:text-sky-400">Be a Rider</a></button>
                 </ul>
 
                 {/* Right - Desktop */}
-                <div className="hidden md:flex gap-4">
-                    <Link to='/login' >
-                        <button className="border border-black px-4 py-1 rounded hover:bg-gray hover:text-black">
-                            Sign In
-                        </button>
-                    </Link>
-                    <Link to='/register'>
-                        <button className="bg-sky-400 text-black px-4 py-1 rounded hover:bg-sky-300">
-                            Register
-                        </button>
-                    </Link>
-                </div>
+                {
+                    user ? (<div className="flex gap-2">
+                        <img className="w-10 rounded-full" src={user.photoURL} alt="" />
+                        <button onClick={handleLogout} className="btn btn-outline btn-error">Logout</button>
+                    </div>) : (
+                        <div className="hidden lg:flex gap-4">
+                            <Link to='/login' >
+                                <button className="btn btn-outline btn-accent">Login</button>
+                            </Link>
+                            <Link to='/register'>
+                                <button className="btn btn-outline btn-info">Register</button>
+                            </Link>
+                        </div>
+                    )
+                }
             </div>
 
             {/* Mobile menu */}
             {open && (
-                <ul className="flex flex-col gap-2 items-center bg-white mt-4 py-4 md:hidden">
-                    <li><a className="hover:text-sky-400">Home</a></li>
-                    <li><a className="hover:text-sky-400">Services</a></li>
-                    <li><a className="hover:text-sky-400">Products</a></li>
-                    <li><a className="hover:text-sky-400">About</a></li>
-                    <li><a className="hover:text-sky-400">Contact</a></li>
+                <ul className="flex flex-col gap-2 items-center bg-white mt-4 py-4 lg:hidden">
+                    <button><a className="hover:text-sky-400">Home</a></button>
+                    <button><a className="hover:text-sky-400">Services</a></button>
+                    <button><a className="hover:text-sky-400">Coverage</a></button>
+                    <button><a className="hover:text-sky-400">TRackOrder</a></button>
+                    <button><a className="hover:text-sky-400">Pricing</a></button>
+                    <button><a className="hover:text-sky-400">Be a Rider</a></button>
                 </ul>
             )}
         </nav>
